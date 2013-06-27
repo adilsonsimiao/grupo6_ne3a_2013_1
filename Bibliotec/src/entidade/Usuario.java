08fa59934887fbf387ce37c5382eac9cd8e19f57
adilsonsimiao <adilsonsimiao@hotmail.com>
2013-06-27 13:58:44 -0300

    #4   Adicionando os atributos  e  o  metodos da classe usuario

--- Bibliotec/src/entidade/Usuario.java
+++ Bibliotec/src/entidade/Usuario.java
@@ -9,5 +9,50 @@ package entidade;
  * @author a1294016
  */
 public class Usuario {
+    private String nome;
+    private int id;
+    private String cpf;
+    private int idEndereco;
+    private String telefone;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getIdEndereco() {
+        return idEndereco;
+    }
+
+    public void setIdEndereco(int idEndereco) {
+        this.idEndereco = idEndereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
     
 }
