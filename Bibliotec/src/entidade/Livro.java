b745e440431a9d55d3b9d0ceafc422f7df964f21
Fabio-darks <fabio.darks@gmail.com>
2013-06-28 20:06:46 -0300

    #10 Criando a entidade do livro

--- Bibliotec/src/entidade/Livro.java
+++ Bibliotec/src/entidade/Livro.java
@@ -9,5 +9,46 @@ package entidade;
  * @author a1294016
  */
 public class Livro {
+    private int id;
+    private String nomeLivro;
+    private String Autor;
+    private int quantidade;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNomeLivro() {
+        return nomeLivro;
+    }
+
+    public void setNomeLivro(String nomeLivro) {
+        this.nomeLivro = nomeLivro;
+    }
+
+    public String getAutor() {
+        return Autor;
+    }
+
+    public void setAutor(String Autor) {
+        this.Autor = Autor;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+    
+    @Override
+     public String toString(){
+        return "livro{"+"id="+id+", nomeLivro="+nomeLivro+", autor"+Autor+ ", quantidade"+quantidade +"}";
+    }
     
 }
