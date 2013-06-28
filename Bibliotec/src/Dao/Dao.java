305850e3aeb2e23b2cfe5f3f4999d979df9ee3b9
Fabio-darks <fabio.darks@gmail.com>
2013-06-28 20:33:48 -0300

    #10 interface Dao genérica pronta

---
+++ Bibliotec/src/Dao/Dao.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Dao;
+
+import java.util.List;
+
+/**
+ *
+ * @author fabio
+ */
+public interface Dao<E> {
+    void persist(E o)  throws Exception;
+    void delete(E o)  throws Exception;
+    E retrieve(int id)  throws Exception;
+    List<E> list()  throws Exception;
+    
+}
