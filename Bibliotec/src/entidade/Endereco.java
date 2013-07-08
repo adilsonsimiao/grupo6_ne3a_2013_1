e5067bb6ebec3005bf6c6a534f0608b126c4b632
Fabio-darks <fabio.darks@gmail.com>
2013-07-07 22:52:02 -0300

    #24 entidade de endereço criada

---
+++ Bibliotec/src/entidade/Endereco.java
@@ -0,0 +1,70 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entidade;
+
+/**
+ *
+ * @author fabio
+ */
+public class Endereco {
+    private String Logradouro;
+    private String Complemento;
+    private int Numero;
+    private String Cidade;
+    private String Uf;
+    private int Cep;
+
+    public String getLogradouro() {
+        return Logradouro;
+    }
+
+    public void setLogradouro(String Logradouro) {
+        this.Logradouro = Logradouro;
+    }
+
+    public String getComplemento() {
+        return Complemento;
+    }
+
+    public void setComplemento(String Complemento) {
+        this.Complemento = Complemento;
+    }
+
+    public int getNumero() {
+        return Numero;
+    }
+
+    public void setNumero(int Numero) {
+        this.Numero = Numero;
+    }
+
+    public String getCidade() {
+        return Cidade;
+    }
+
+    public void setCidade(String Cidade) {
+        this.Cidade = Cidade;
+    }
+
+    public String getUf() {
+        return Uf;
+    }
+
+    public void setUf(String Uf) {
+        this.Uf = Uf;
+    }
+
+    public int getCep() {
+        return Cep;
+    }
+
+    public void setCep(int Cep) {
+        this.Cep = Cep;
+    }
+
+    
+    
+    
+}
