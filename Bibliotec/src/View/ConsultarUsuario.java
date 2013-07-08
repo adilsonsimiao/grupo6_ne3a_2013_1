61284341d8dd43e7b37d5cbfad7ee5e4794f88b7
Fabio-darks <fabio.darks@gmail.com>
2013-07-08 15:53:36 -0300

    #23 java atualizado

--- Bibliotec/src/View/ConsultarUsuario.java
+++ Bibliotec/src/View/ConsultarUsuario.java
@@ -162,28 +162,35 @@ public class ConsultarUsuario extends javax.swing.JFrame {
 
     private void jBConsultarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBConsultarActionPerformed
         if (jRBNome.isSelected()) {
+
+
             try {
-                usuario = (Usuario) daoUsuario.listNome(jRBNome.getText());
-                tableModel = (DefaultTableModel) jTAreaDadosConsulta.getModel(); 
-                tableModel.addRow(new Object[]{usuario.getId(), usuario.getNome(), usuario.getCpf(), usuario.getTelefone(), usuario.getIdEndereco()});
-                jTAreaDadosConsulta.setModel(tableModel);
+                listaUsuario = daoUsuario.list();
                 listaUsuario.add(usuario);
+                tableModel = (DefaultTableModel) jTAreaDadosConsulta.getModel();
+                tableModel.addRow(new Object[]{usuario.getId(), usuario.getNome(), usuario.getCpf(), usuario.getTelefone(), usuario.getIdEndereco()});
+                this.jTAreaDadosConsulta.setModel(tableModel);
+                
+                
             } catch (Exception ex) {
                 Logger.getLogger(ConsultarUsuario.class.getName()).log(Level.SEVERE, null, ex);
             }
-        }else if (jRBCpf.isSelected()){
+
+
+
+        } else if (jRBCpf.isSelected()) {
             try {
                 usuario = (Usuario) daoUsuario.listNome(jRBCpf.getText());
-                tableModel = (DefaultTableModel) jTAreaDadosConsulta.getModel(); 
+                tableModel = (DefaultTableModel) jTAreaDadosConsulta.getModel();
                 tableModel.addRow(new Object[]{usuario.getId(), usuario.getNome(), usuario.getCpf(), usuario.getTelefone(), usuario.getIdEndereco()});
                 jTAreaDadosConsulta.setModel(tableModel);
                 listaUsuario.add(usuario);
             } catch (Exception ex) {
                 Logger.getLogger(ConsultarUsuario.class.getName()).log(Level.SEVERE, null, ex);
             }
-        }else{
-        JOptionPane.showMessageDialog(null, "selecione um campo");
-    }
+        } else {
+            JOptionPane.showMessageDialog(null, "selecione um campo");
+        }
     }//GEN-LAST:event_jBConsultarActionPerformed
 
     private void jBFecharActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBFecharActionPerformed
