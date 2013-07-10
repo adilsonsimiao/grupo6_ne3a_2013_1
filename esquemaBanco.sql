4f9882bb8ed208b9a40c3e9c440e1fc80c34a90f
Fabio-darks <fabio.darks@gmail.com>
2013-07-10 16:57:04 -0300

    esquema do banco sql

--- esquemaBanco.sql
+++ esquemaBanco.sql
@@ -2,24 +2,32 @@ DROP DATABASE IF EXISTS bibliotec;
 CREATE DATABASE IF NOT EXISTS bibliotec;
 USE bibliotec;
 DROP TABLE IF EXISTS Endereco;
+
+CREATE TABLE Cidade(
+		id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+        nome varchar(100) NOT NULL,
+        uf char(2) NOT NULL);
+
+
 CREATE TABLE Endereco
 		(id INT NOT NULL AUTO_INCREMENT,
 		numero INT NOT NULL,
 		complemento VARCHAR(45),
 		cep VARCHAR(8) NOT NULL,
-		logradouro INT,
-		cidade varchar (200),
-		PRIMARY KEY (id));
+		logradouro VARCHAR(200) NOT NULL,
+		idCidade INT NOT NULL,
+		PRIMARY KEY (id),
+		FOREIGN KEY (idCidade) REFERENCES Cidade(id));
 
 DROP TABLE IF EXISTS Usuario;
 CREATE TABLE Usuario
 		(id INT NOT NULL AUTO_INCREMENT,
 		nome VARCHAR(100) NOT NULL,
 		cpf VARCHAR(11) UNIQUE NOT NULL,
-		idEndereco int,
+		idEndereco INT NOT NULL,
 		telefone varchar(11) NOT NULL,
 		PRIMARY KEY (id),
-        FOREIGN KEY (idEndereco) REFERENCES Endereco(id) ON DELETE CASCADE);
+        	FOREIGN KEY (idEndereco) REFERENCES Endereco(id) ON DELETE CASCADE);
 
 DROP TABLE IF EXISTS Autor;
 CREATE TABLE Autor
@@ -33,8 +41,7 @@ CREATE TABLE Livro
        (id INT NOT NULL AUTO_INCREMENT,
         nomeLivro VARCHAR(150) NOT NULL,
         quantidade INT NOT NULL,
-        autor INT NOT NULL,
+        idAutor INT NOT NULL,
         PRIMARY KEY (id),
-        FOREIGN KEY(autor) REFERENCES Autor(id) ON DELETE CASCADE);
+        FOREIGN KEY(idAutor) REFERENCES Autor(id) ON DELETE CASCADE);
 
-INSERT INTO Usuario (id, nome, cpf, idEndereco, telefone) VALUES(null, 'Eduardo', '0000000011', '0', '4498989898');
