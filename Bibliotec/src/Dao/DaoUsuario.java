9bb61ed8eaa86bb2d16521434485957ffafd985d
Fabio-darks <fabio.darks@gmail.com>
2013-07-10 13:42:27 -0300

    #4 modificando o dao usuario

--- Bibliotec/src/Dao/DaoUsuario.java
+++ Bibliotec/src/Dao/DaoUsuario.java
@@ -16,8 +16,8 @@ import java.util.List;
  *
  * @author a1294016
  */
-public class DaoUsuario implements Dao<Usuario>{
-    
+public class DaoUsuario implements Dao<Usuario> {
+
     private static Usuario converteRsParaUsuario(ResultSet rs) throws SQLException {
         Usuario u = new Usuario();
         u.setId(rs.getInt("id"));
@@ -26,104 +26,110 @@ public class DaoUsuario implements Dao<Usuario>{
         u.setTelefone(rs.getString("telefone"));
         u.setIdEndereco(rs.getInt("idEndereco"));
         return u;
-        
+
     }
 
     @Override
     public void persist(Usuario u) throws Exception {
-        if(u.getId()==0)
+        if (u.getId() == 0) {
             insert(u);
-        else
+        } else {
             update(u);
+        }
     }
 
     @Override
     public void delete(Usuario u) throws Exception {
-        Statement st = ConnectionFactory.prepareConnection().createStatement();                                
+        Statement st = ConnectionFactory.prepareConnection().createStatement();
         st.execute("DELETE FROM Usuario WHERE id = " + u.getId());
     }
 
     @Override
     public Usuario retrieve(int id) throws Exception {
-           Statement st = ConnectionFactory.prepareConnection().createStatement();                           
+        Statement st = ConnectionFactory.prepareConnection().createStatement();
         st.execute("SELECT * FROM Usuario WHERE id = " + id);
         ResultSet rs = st.getResultSet();
-        
+
         rs.next();
         Usuario p = converteRsParaUsuario(rs);
-       
+
         return p;
-        
+
     }
 
     @Override
     public List<Usuario> list() throws Exception {
-        List<Usuario> Usuarios = new ArrayList<Usuario>();
-        
-         Statement st = ConnectionFactory.prepareConnection().createStatement();                             
-        ResultSet rs =  st.executeQuery("SELECT * FROM Usuario");
-        
-        while(rs.next()){
+        List<Usuario> Usuarios = new ArrayList<>();
+
+        Statement st = ConnectionFactory.prepareConnection().createStatement();
+        ResultSet rs = st.executeQuery("SELECT * FROM Usuario");
+
+        while (rs.next()) {
             Usuario p = converteRsParaUsuario(rs);
             Usuarios.add(p);
         }
-        
+
         return Usuarios;
     }
-    public List<Usuario> listNome(String nome) throws Exception {
-        List<Usuario> Usuarios = new ArrayList<>();
-        
-         Statement st = ConnectionFactory.prepareConnection().createStatement();                             
-        ResultSet rs =  st.executeQuery("SELECT * FROM Usuario WHERE nome = '"+nome+"'");
-        
-        while(rs.next()){
+
+    public Usuario listNome(String nome) throws Exception {
+        Usuario Usuarios = new Usuario();
+
+        Statement st = ConnectionFactory.prepareConnection().createStatement();
+        ResultSet rs = st.executeQuery("SELECT * FROM Usuario WHERE nome = '" + nome + "'");
+
+        while (rs.next()) {
             Usuario p = converteRsParaUsuario(rs);
-            Usuarios.add(p);
+            Usuarios = (p);
         }
-        
+
         return Usuarios;
     }
-    public List<Usuario> listCpf(String cpf) throws Exception {
-        List<Usuario> Usuarios = new ArrayList<>();
-        
-         Statement st = ConnectionFactory.prepareConnection().createStatement();                             
-        ResultSet rs =  st.executeQuery("SELECT * FROM Usuario WHERE cpf = '"+cpf+"'");
-        
-        while(rs.next()){
+
+    public Usuario listCpf(String cpf) throws Exception {
+       Usuario Usuarios = new Usuario();
+
+        Statement st = ConnectionFactory.prepareConnection().createStatement();
+        ResultSet rs = st.executeQuery("SELECT * FROM Usuario WHERE nome = '" + cpf + "'");
+
+        while (rs.next()) {
             Usuario p = converteRsParaUsuario(rs);
-            Usuarios.add(p);
+            Usuarios = (p);
         }
-        
+
         return Usuarios;
     }
 
     private void update(Usuario u) throws SQLException {
-        PreparedStatement pst =  ConnectionFactory.prepareConnection().prepareStatement("UPDATE INTO Usuario (id, nome, cpf, telefone, idEndereco) VALUES(?,?,?,?)", Statement.RETURN_GENERATED_KEYS);
+        PreparedStatement pst = ConnectionFactory.prepareConnection().prepareStatement("UPDATE INTO Usuario (id, nome, cpf, telefone, idEndereco) VALUES(?,?,?,?)", Statement.RETURN_GENERATED_KEYS);
         pst.setInt(1, u.getId());
         pst.setString(2, u.getNome());
         pst.setString(3, u.getCpf());
         pst.setString(4, u.getTelefone());
         pst.setInt(5, u.getIdEndereco());
-        
-        
+
+
         pst.execute();
     }
 
     private void insert(Usuario u) throws SQLException {
-        PreparedStatement pst =  ConnectionFactory.prepareConnection().prepareStatement("INSERT INTO Usuario (id, nome, cpf, telefone, idEndereco) VALUES(?,?,?,?)", Statement.RETURN_GENERATED_KEYS);
+        PreparedStatement pst = ConnectionFactory.prepareConnection().prepareStatement("INSERT INTO Usuario (id, nome, cpf, telefone, idEndereco) VALUES(?,?,?,?)", Statement.RETURN_GENERATED_KEYS);
         pst.setInt(1, u.getId());
         pst.setString(2, u.getNome());
         pst.setString(3, u.getCpf());
         pst.setString(4, u.getTelefone());
         pst.setInt(5, u.getIdEndereco());
-        
-        
+
+
         pst.execute();
 
         ResultSet rs = pst.getGeneratedKeys();
         rs.next();
         u.setId(rs.getInt(1));
     }
-    
+    public static void main(String[] args) {
+        
+    }
+
     
 }
