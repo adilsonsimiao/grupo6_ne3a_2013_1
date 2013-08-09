cd9b4731954a18b6775fa88bd2f4a9b33cffc3de
Fabio-darks <fabio.darks@gmail.com>
2013-08-08 22:26:57 -0300

    #37 montando a entidade de devolução

--- Bibliotec/src/entidade/Devolucao.java
+++ Bibliotec/src/entidade/Devolucao.java
@@ -4,10 +4,56 @@
  */
 package entidade;
 
+import java.util.Calendar;
+
 /**
  *
  * @author fabio
  */
 public class Devolucao {
+    private int id;
+    private int idEmprestimo;
+    private Emprestimo emprestimo;
+    private Calendar dataDevolução;
     
+    public Devolucao(){
+        
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdEmprestimo() {
+        return idEmprestimo;
+    }
+
+    public void setIdEmprestimo(int idEmprestimo) {
+        this.idEmprestimo = idEmprestimo;
+    }
+
+    public Emprestimo getEmprestimo() {
+        return emprestimo;
+    }
+
+    public void setEmprestimo(Emprestimo emprestimo) {
+        this.emprestimo = emprestimo;
+    }
+
+    public Calendar getDataDevolução() {
+        return dataDevolução;
+    }
+
+    public void setDataDevolução(Calendar dataDevolução) {
+        this.dataDevolução = dataDevolução;
+    }
+    
+    @Override
+    public String toString() {
+        return "Devolução{" + ", id=" + id +  ", idEmprestimo=" + idEmprestimo + ", emprestimo=" + emprestimo + ", dataDevolução=" + dataDevolução +  '}';
+    }
 }
