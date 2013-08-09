cdd7591edb53dd9c4c5dec4e4afb139984e8fdf1
Fabio-darks <fabio.darks@gmail.com>
2013-08-08 22:30:25 -0300

    #32 construindo a entidade editora

--- Bibliotec/src/entidade/Editora.java
+++ Bibliotec/src/entidade/Editora.java
@@ -9,5 +9,50 @@ package entidade;
  * @author a1294016
  */
 public class Editora {
-    
+    private int id;
+    private String nome;
+    private String email;
+    private Endereco endereco;
+   
+    public Editora(){
+        
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+   
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+    @Override
+    public String toString() {
+        return "Editora{" + "nome=" + nome + ", id=" + id + ", endereco=" + endereco + ", Email=" + email +'}';
+    }
 }
