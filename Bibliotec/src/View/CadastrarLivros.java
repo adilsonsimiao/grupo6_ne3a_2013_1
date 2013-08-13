5e429978e9dec7d9a7cb54bff398f287eb58d2a7
Fabio-darks <fabio.darks@gmail.com>
2013-08-12 22:02:22 -0300

    #28 alteração do nome da classe de cadastro de livros

--- Bibliotec/src/View/CadastrarLivros.java
+++ Bibliotec/src/View/CadastrarLivros.java
@@ -8,12 +8,12 @@ package View;
  *
  * @author a1294016
  */
-public class CadastroLivros extends javax.swing.JFrame {
+public class CadastrarLivros extends javax.swing.JFrame {
 
     /**
-     * Creates new form CadastroLivros
+     * Creates new form CadastrarLivros
      */
-    public CadastroLivros() {
+    public CadastrarLivros() {
         initComponents();
     }
 
@@ -233,20 +233,20 @@ public class CadastroLivros extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(CadastroLivros.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CadastrarLivros.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(CadastroLivros.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CadastrarLivros.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(CadastroLivros.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CadastrarLivros.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(CadastroLivros.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CadastrarLivros.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new CadastroLivros().setVisible(true);
+                new CadastrarLivros().setVisible(true);
             }
         });
     }
