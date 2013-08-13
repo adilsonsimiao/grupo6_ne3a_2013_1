c712e25f0af11c02f2571ef86200327e2d735973
Fabio-darks <fabio.darks@gmail.com>
2013-08-12 22:27:54 -0300

    #38 atribuindo a função para o botão cancelar

--- Bibliotec/src/View/DevolverLivro.java
+++ Bibliotec/src/View/DevolverLivro.java
@@ -72,6 +72,11 @@ public class DevolverLivro extends javax.swing.JFrame {
         );
 
         jButton2.setText("Cancelar");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
 
         jButton1.setText("Devolver");
 
@@ -126,6 +131,10 @@ public class DevolverLivro extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        dispose();
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
