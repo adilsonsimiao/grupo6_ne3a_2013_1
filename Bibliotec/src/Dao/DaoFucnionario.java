612bc5188721464b20c483c98f86b65e147fc769
fabio-darks <fabio.darks@gmail.com>
2013-09-19 22:06:12 -0300

    #98 comit criando o DaoFuncionario para realizar list com consultas sql no banco específicas

---
+++ Bibliotec/src/Dao/DaoFucnionario.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Dao;
+
+import entidade.Funcionario;
+import hibernate.HibernateDao;
+import java.util.List;
+
+/**
+ *
+ * @author fabio
+ */
+public class DaoFucnionario extends HibernateDao<Funcionario> {
+
+    public DaoFucnionario() {
+        c = Funcionario.class;
+    }
+
+    public List<Funcionario> retrieveByNome(String nome) {
+        return createNamedQuery("retrieveByNomeFuncionario").setParameter("nome", "%" + nome + "%").getResultList();
+
+    }
+
+    public List<Funcionario> retrieveByCpf(String cpf) {
+        return createNamedQuery("retrieveByCpfFuncionario").setParameter("cpf", "%" + cpf + "%").getResultList();
+    }
+}
